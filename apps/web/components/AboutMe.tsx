2b2c5398ef2a716f709cc608ca2114309fb04678
tnhnkzc <tunahankuzucuu@gmail.com>
2023-04-09 17:12:11 +0200

    feat: add intersection observer on preview component

--- apps/web/components/AboutMe.tsx
+++ apps/web/components/AboutMe.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+
 type AboutMeProps = {
   aboutMe: String;
 };
@@ -8,11 +8,9 @@ export default class AboutMe extends Component<AboutMeProps> {
   render() {
     const { aboutMe } = this.props;
     return (
-      <Wrap className="aboutMe">
-        <WrapItem>
-          <p>{aboutMe}</p>
-        </WrapItem>
-      </Wrap>
+      <div className="aboutMe">
+        <p>{aboutMe}</p>
+      </div>
     );
   }
 }
--- apps/web/components/preview/Preview.tsx
+++ apps/web/components/preview/Preview.tsx
@@ -1,4 +1,4 @@
-import { Box,Wrap,WrapItem } from "@chakra-ui/react";
+import { Box, Container, Divider } from "@chakra-ui/react";
 import React from "react";
 import Identity from "../Identity";
 import SkillsList from "../SkillsList";
@@ -10,6 +10,7 @@ import SoftSkillsList from "../SoftSkillsList";
 import WorkExperienceList from "../WorkExperiencesList";
 import { resumeJson } from "../../pages/resume-json";
 import AboutMe from "../AboutMe";
+import { useInView } from "react-intersection-observer";
 
 export default function Preview() {
   const {
@@ -26,23 +27,90 @@ export default function Preview() {
     workExperiences,
   } = resumeJson;
 
+  const { ref, inView } = useInView();
+
+  console.log("inView ? ", inView);
 
   return (
-    <Box maxW="md">
+    <Box padding="4px" maxH="full">
       <pre>
-        <Identity
-          name={name}
-          headline={headline}
-          yearsOfExperience={yearsOfExperience}
-        />
-        <SkillsList {...{ skills }} />
-        <LanguagesList languages={languages} />
-        <HobbiesList hobbies={hobbies} />
-        <AboutMe aboutMe={aboutMe} />
-        <TimelineList timeline={timeline} />
-        <EducationsList educations={educations} />
-        <SoftSkillsList softSkills={softSkills} />
-        <WorkExperienceList workExperiences={workExperiences} />
+        <Container ref={ref} h="5px" w="5px">
+        </Container>
+        <Container bg="red.400" h="calc(100vh)">
+          <Identity
+            name={name}
+            headline={headline}
+            yearsOfExperience={yearsOfExperience}
+          />
+
+          <Identity
+            name={name}
+            headline={headline}
+            yearsOfExperience={yearsOfExperience}
+          />
+          <Identity
+            name={name}
+            headline={headline}
+            yearsOfExperience={yearsOfExperience}
+          />
+          <Identity
+            name={name}
+            headline={headline}
+            yearsOfExperience={yearsOfExperience}
+          />
+          <Identity
+            name={name}
+            headline={headline}
+            yearsOfExperience={yearsOfExperience}
+          />
+          <Identity
+            name={name}
+            headline={headline}
+            yearsOfExperience={yearsOfExperience}
+          />
+          <Identity
+            name={name}
+            headline={headline}
+            yearsOfExperience={yearsOfExperience}
+          />
+          <Identity
+            name={name}
+            headline={headline}
+            yearsOfExperience={yearsOfExperience}
+          />
+          <Identity
+            name={name}
+            headline={headline}
+            yearsOfExperience={yearsOfExperience}
+          />
+          <Identity
+            name={name}
+            headline={headline}
+            yearsOfExperience={yearsOfExperience}
+          />
+          <Identity
+            name={name}
+            headline={headline}
+            yearsOfExperience={yearsOfExperience}
+          />
+          <Identity
+            name={name}
+            headline={headline}
+            yearsOfExperience={yearsOfExperience}
+          />
+          <Identity
+            name={name}
+            headline={headline}
+            yearsOfExperience={yearsOfExperience}
+          />
+        </Container>
+        {!inView ? (
+          <Container bg="yellow.400" h="calc(100vh - 60px)" marginTop="10px">
+            <TimelineList timeline={timeline} />
+            <EducationsList educations={educations} />
+            <SoftSkillsList softSkills={softSkills} />
+          </Container>
+        ) : null}
       </pre>
     </Box>
   );
