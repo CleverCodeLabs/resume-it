db4847b925c5fd335da56edfc65a56d071172b2f
Philippe Wronski <philippe.wronski@gmail.com>
2023-05-02 18:09:05 +0200

    feat: add prisma and mongodb connection

--- README.md
+++ README.md
@@ -44,16 +44,19 @@ pnpm run dev
 
 ### Remote Caching
 
-Turborepo can use a technique known as [Remote Caching](https://turbo.build/repo/docs/core-concepts/remote-caching) to share cache artifacts across machines, enabling you to share build caches with your team and CI/CD pipelines.
+Turborepo can use a technique known as [Remote Caching](https://turbo.build/repo/docs/core-concepts/remote-caching) to
+share cache artifacts across machines, enabling you to share build caches with your team and CI/CD pipelines.
 
-By default, Turborepo will cache locally. To enable Remote Caching you will need an account with Vercel. If you don't have an account you can [create one](https://vercel.com/signup), then enter the following commands:
+By default, Turborepo will cache locally. To enable Remote Caching you will need an account with Vercel. If you don't
+have an account you can [create one](https://vercel.com/signup), then enter the following commands:
 
 ```
 cd my-turborepo
 pnpm dlx turbo login
 ```
 
-This will authenticate the Turborepo CLI with your [Vercel account](https://vercel.com/docs/concepts/personal-accounts/overview).
+This will authenticate the Turborepo CLI with
+your [Vercel account](https://vercel.com/docs/concepts/personal-accounts/overview).
 
 Next, you can link your Turborepo to your Remote Cache by running the following command from the root of your turborepo:
 
@@ -71,3 +74,42 @@ Learn more about the power of Turborepo:
 - [Filtering](https://turbo.build/repo/docs/core-concepts/monorepos/filtering)
 - [Configuration Options](https://turbo.build/repo/docs/reference/configuration)
 - [CLI Usage](https://turbo.build/repo/docs/reference/command-line-reference)
+
+## Docker
+
+### Start database (MongoDB)
+
+run the following commands into the root directory:
+
+```
+docker run --name mongodb -d --rm -p 27017:27017 \
+    -v $(pwd)/data:/data/db \
+    mongo:latest mongod --replSet rs0
+```
+
+```
+docker exec -it mongodb mongosh --eval "rs.initiate({_id: \"rs0\",members: [{_id: 0, host: \"localhost:27017\"}]})"
+```
+
+or run `docker-compose up -d` into the root directory.
+
+### Connect to database
+
+TODO
+
+### Modify database schema
+
+run the following command from `apps/api` directory:
+
+```
+pnpm dlx prisma db push 
+```
+
+### Seeding database
+
+run the following command from anywhere:
+
+```
+pnpm run db:seed --filter=api  
+```
+
---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,129 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const resume = await prisma.resume.create({
+    data: {
+      fullName: 'Philippe Wronski',
+      headline: 'Tech Lead / Lead Dev',
+      yearsOfExperience: 12,
+      aboutMe: `Lead Technique, j\'ai travaillé sur des sujets à fort challenge autour des technologies Java/JEE (Web/mobile) et Front dans divers domaines : Industrie automobile / Bâtiment / Transport ferroviaire, Assurance et Energie.\n
+        Fort de 12 ans d\'expérience, mon parcours dans le domaine de l\'Informatique applicative, m\'a permis d\'acquérir une forte compétence dans la conception, le développement, l’innovation et l’écoute du client.\n
+        Depuis 4 ans, j’occupe le rôle de lead technique dans lequel j’ai pu développer mon appétence pour l’agilité, l’automatisation (DevOps) et le webdesign.`,
+      skills: [
+        {
+          name: 'React',
+          isPrincipal: true,
+        },
+        {
+          name: 'Node.js',
+          isPrincipal: true,
+        },
+        {
+          name: 'Java',
+          isPrincipal: false,
+        },
+        {
+          name: 'Spring',
+          isPrincipal: false,
+        },
+      ],
+      languages: [
+        {
+          name: 'Français',
+          level: 'Maternelle',
+        },
+      ],
+      hobbies: ['VTT', 'Jeux Vidéo', 'Lecture', 'Randonnée'],
+      educations: [
+        {
+          name: 'Licence Informatique',
+          nameInstitution: 'CNAM Belfort',
+          year: 2003,
+        },
+        {
+          name: 'DUT Information',
+          nameInstitution: 'IUT Belfort',
+          year: 2002,
+        },
+      ],
+      softSkills: [
+        {
+          name: 'Compétences organisationnelles',
+          value: 4,
+        },
+        {
+          name: 'Compétences relationnelles',
+          value: 4.5,
+        },
+        {
+          name: 'Compétences personnelles',
+          value: 4,
+        },
+        {
+          name: 'Engagement au travail',
+          value: 4.5,
+        },
+        {
+          name: 'Compétences managériales',
+          value: 3,
+        },
+      ],
+      workExperiences: [
+        {
+          companyName: 'BforBank',
+          headLine: 'Lead Tech',
+          duration: '1an et 2mois',
+          numberOfTeamMembers: 12,
+          projectTitle:
+            'Conception de l’architecture et développement du CRM pour la nouvelle offre bancaire de Bforbank',
+          tasks: [
+            'Création d’ADR pour l’architecture et les technologies utilisées.',
+            'Mise en place d’un architecture Micro Frontend.',
+            'Participation au DSM, Grooming (hebdomadaire), Review (toutes les 2 semaines)',
+            'Suivi de production et livraison en production',
+            'Création de composants respectants la nouvelle charte graphique',
+            'tâches CI/CD, utilisation de Gitlab CI',
+            'Conceptions technique et fonctionnelle en relation avec le client.',
+            'Réalisation de tests unitaires et d’intégration',
+            'Réalisation de documentation technique et d’architecture.',
+            'Relectures, cadrage techniques de l’équipe.',
+            'Suivi des déploiement, analyse et correction d’anomalie.',
+          ],
+          technicalEnvironment: [
+            {
+              name: 'ENVIRONNEMENT',
+              description:
+                'GCP,Docker,Kubernetes,Helm,Typescript,Node.js,Nx,Gitlab CI,Git OPS',
+            },
+            {
+              name: 'BACKEND',
+              description: 'Nest.js,PostgreSQL,Kafka,Kafka Connect,Jest',
+            },
+            {
+              name: 'FRONTEND',
+              description:
+                'Rest.js, Jest / React testing library / Cypress, Storybook, Material UI',
+            },
+            {
+              name: 'AGILITE',
+              description: 'Safe',
+            },
+          ],
+        },
+      ],
+    },
+  });
+  console.log('Resume created', resume);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
