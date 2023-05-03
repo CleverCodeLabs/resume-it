c8d2d8ff092107f1f09fa71f48d21f24baa9023c
Philippe Wronski <philippe.wronski@gmail.com>
2023-05-03 15:02:23 +0200

    feat: add CRUD for resume

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ResumeModule } from './resume/resume.module';
 
 @Module({
-  imports: [DatabaseModule, ResumeModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, ResumeModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/resume/resume.service.ts
+++ apps/api/src/resume/resume.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from '../database/prisma.service';
 
 @Injectable()
@@ -6,9 +7,25 @@ export class ResumeService {
   constructor(private prisma: PrismaService) {}
 
   //TODO change type on resumeInput params
-  createResume(resume: any): any {
+  async createResume(resume: Prisma.ResumeCreateInput): Promise<any> {
     return this.prisma.resume.create({
       data: { ...resume },
     });
+  }
+
+  async updateResume(id: string, resume: Prisma.ResumeUpdateInput) {
+    return this.prisma.resume.update({ data: { ...resume }, where: { id } });
+  }
+
+  async resumes() {
+    return this.prisma.resume.findMany();
+  }
+
+  async resume(id: string) {
+    return this.prisma.resume.findUniqueOrThrow({ where: { id } });
+  }
+
+  async deleteResume(id: string) {
+    return this.prisma.resume.delete({ where: { id } });
   }
 }
