5437ee46ce756d85d66fb66860edc1e1aec06857
Philippe Wronski <philippe.wronski@gmail.com>
2023-05-03 10:02:01 +0200

    feat: create resume endpoint and configure swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Resume-it OpenAPI')
+    .setDescription('The Resume-it API')
+    .setVersion('1.0')
+    .addTag('resumes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
