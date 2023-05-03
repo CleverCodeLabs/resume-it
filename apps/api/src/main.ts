e8a619b4b11a5963af5e7f1c44392868638d0006
Philippe Wronski <philippe.wronski@gmail.com>
2023-05-03 15:21:02 +0200

    feat: add validation to resume endpoint

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Resume-it OpenAPI')
     .setDescription('The Resume-it API')
@@ -14,7 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3000);
+  await app.listen(3333);
 }
 
 bootstrap();
