1536df5d968f363e8123b6dd7ab3c1cfe1fd3ad3
Joëlle MANETY <joelle.mantey@gmail.com>
2023-05-05 12:07:12 +0200

    feat: use resumes endpoint to provide data

--- apps/web/pages/resume/dashboard.tsx
+++ apps/web/pages/resume/dashboard.tsx
@@ -5,24 +5,22 @@ import Layout from "../../components/layouts/Layout";
 import { NextPageWithLayout } from "../_app";
 import ItemResume from "../../components/dashboard/ItemResume";
 import { IoAddOutline } from "react-icons/io5";
+import useSWR from "swr";
+import axios from "axios";
 
 const Dashboard: NextPageWithLayout = () => {
   const router = useRouter();
-  const resumes = [
-    {
-      headline: "Tech Lead / Lead Dev",
-      yearsOfExperience: 12,
-    },
-    {
-      headline: "Lead Dev",
-      yearsOfExperience: 12,
-    },
-  ];
+  const fetcher = (url: any) => axios.get(url).then((res) => res.data);
+
+  const { data, error } = useSWR("http://localhost:3333/resumes", fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
 
   const handleClick = () => {
     router.push("/resume/editing");
   };
-  const listItems = resumes.map((resume: any) => (
+  const listItems = data.map((resume: any) => (
     <ItemResume
       key={resume.headline}
       headline={resume.headline}
