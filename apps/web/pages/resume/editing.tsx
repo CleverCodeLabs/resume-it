1d2ea84d7bb108f69068ef30f4693a6bab30ddd0
Joëlle MANETY <joelle.mantey@gmail.com>
2023-05-05 10:56:52 +0200

    feat: move resume forme in editing page

--- apps/web/pages/resume/editing.tsx
+++ apps/web/pages/resume/editing.tsx
@@ -1,10 +1,28 @@
-import { ReactElement } from "react";
+import React, { ReactElement } from "react";
 import Layout from "../../components/layouts/Layout";
 import ResumeEditingLayout from "../../components/layouts/ResumeEditingLayout";
 import { NextPageWithLayout } from "../_app";
+import { Box } from "@chakra-ui/react";
+import ContactForm from "../../components/contact/ContactForm";
+import ProfileForm from "../../components/profile/ProfileForm";
+import LocationForm from "../../components/location/LocationForm";
+import Languages from "../../components/languages/Languages";
+import Hobbies from "../../components/hobbies/Hobbies";
+import Skills from "../../components/skills/Skills";
+import Networks from "../../components/networks/Networks";
 
 const Editing: NextPageWithLayout = () => {
-  return <div>Le contenue de mon formulaire</div>;
+  return (
+    <Box>
+      <ContactForm />
+      <ProfileForm />
+      <LocationForm />
+      <Languages />
+      <Hobbies />
+      <Skills />
+      <Networks />
+    </Box>
+  );
 };
 
 Editing.getLayout = function getLayout(page: ReactElement) {
