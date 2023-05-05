dad71081a676c71674be2d2d5b9e076395fbd62d
Joëlle MANETY <joelle.mantey@gmail.com>
2023-05-05 12:07:12 +0200

    feat: use resumes endpoint to provide data

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: ['http://localhost:3000'] });
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Resume-it OpenAPI')
