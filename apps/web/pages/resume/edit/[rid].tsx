6e8ddcb4ff3b256fc63d8a3d0ad8a5e41aeaadfa
Philippe Wronski <philippe.wronski@gmail.com>
2023-05-09 13:38:57 +0200

    feat: merge contact and profile part and use profile context

--- apps/web/pages/resume/edit/[rid].tsx
+++ apps/web/pages/resume/edit/[rid].tsx
@@ -58,9 +58,9 @@ const Editing: NextPageWithLayout = () => {
             overflowY="scroll"
             h="calc(100vh - 60px)"
             scrollBehavior="smooth"
-            >
+          >
             <ProfileForm
-            {...{
+              {...{
                 picture,
                 fullName,
                 phoneNumber,
@@ -70,7 +70,8 @@ const Editing: NextPageWithLayout = () => {
                 yearsOfExperience,
                 aboutMe,
               }}
-            /><LocationForm />
+            />
+            <LocationForm />
             <Languages />
             <Hobbies />
             <Skills />
